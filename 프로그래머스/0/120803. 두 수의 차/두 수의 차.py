428daeed0dc4c4a9abcd0a2cdee6a36c19c74fca
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-09-02 11:17:54 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
@@ -1,3 +1,9 @@
 def solution(num1, num2):
-    answer = num1 - num2
+    def validate_input(x, lower, upper):
+        if x >= lower and x <= upper:
+            return True
+        return False
+    num_list = [num1, num2]
+    assert all([validate_input(x, -50000, 50000) for x in num_list])
+    answer = num1 -num2
     return answer
