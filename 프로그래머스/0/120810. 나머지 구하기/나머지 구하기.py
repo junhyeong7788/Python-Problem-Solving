c3e3083f49ef9e285a14a4df97ad379c7fec920d
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-09-02 17:37:35 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    def validate_input(x, lower, upper):
+        if x > lower and x<= upper:
+            return True
+        return False
+    num_list = [num1, num2]
+    assert all([validate_input(x, 0, 100) for x in num_list])
+    answer = num1 % num2
+    return answer
