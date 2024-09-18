413e034d45d45b2bf3290cfaadbf277abb51ed39
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-09-18 23:08:40 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -2,6 +2,6 @@ def solution(n, k):
     lambSkewers = 12000
     drink = 2000
     payment = (lambSkewers * n) + (drink * k)
-    discount = (n//10) * 2000
+    discount = (n//10) * drink
     
     return payment-discount if n >= 10 else payment
