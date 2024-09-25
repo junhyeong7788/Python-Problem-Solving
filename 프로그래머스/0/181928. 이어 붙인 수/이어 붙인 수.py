52ea9f394d84006ea899aff089fb45e5fb67b919
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-09-25 23:49:30 +0900

    Update 이어 붙인 수.py

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -2,7 +2,3 @@ def solution(num_list):
     odd_num = [i for i in num_list if i%2 == 1]
     even_num = [i for i in num_list if i%2 == 0]
     return int(''.join(map(str, even_num))) + int(''.join(map(str, odd_num)))
-
-
-    
-            
