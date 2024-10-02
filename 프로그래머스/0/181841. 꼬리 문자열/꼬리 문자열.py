a13c10becc354d4863e5818ba04177123544a2a9
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-10-03 03:24:17 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,8 +1,4 @@
 def solution(str_list, ex):
-    new_list = []
-    for i in str_list:
-        if ex not in i:
-            new_list.append(i)
-    return ''.join(new_list)
+    return ''.join(i for i in str_list if ex not in i)
 
 
