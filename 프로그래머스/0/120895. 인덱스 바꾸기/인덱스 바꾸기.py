17c924ed66fef8c9188f15b1ad5de07f15ace049
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-10-11 12:39:04 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,10 +1,4 @@
 def solution(my_string, num1, num2):
-    answer = []
-    for i, v in enumerate(my_string):
-        if i == num2:
-            answer.append(my_string[num1])
-        elif i == num1:
-            answer.append(my_string[num2])
-        else:
-            answer.append(v)
-    return ''.join(answer)
+    strList = list(my_string)
+    strList[num1], strList[num2] = strList[num2], strList[num1]
+    return ''.join(strList)
