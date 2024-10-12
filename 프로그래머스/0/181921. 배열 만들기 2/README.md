c5ce41742f5e6e3924a9281d5d31fd29ba58f35f
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-10-12 15:40:38 +0900

    [level 0] Title: 배열 만들기 2, Time: 723.95 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/README.md
+++ 프로그래머스/0/181921. 배열 만들기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 2 - 181921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 491.97 ms
+메모리: 10.3 MB, 시간: 723.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 15:39:33
+2024년 10월 12일 15:40:36
 
 ### 문제 설명
 
--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,10 +1,6 @@
 def solution(l, r):
     answer = []
-    
-    for i in range(l, r+1):
-        if all(num in ['0', '5'] for num in str(i)):
-            answer.append(i)
-    
-    if len(answer) == 0:
-        answer.append(-1)
-    return answer
+    for num in range(l, r + 1):
+        if not set(str(num)) - set(['0', '5']):
+            answer.append(num)
+    return answer if answer else [-1]
