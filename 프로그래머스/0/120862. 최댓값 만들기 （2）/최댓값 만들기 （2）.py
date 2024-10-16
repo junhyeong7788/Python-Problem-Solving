501d17109ba71c9e7f740189c08ae3bd458a5f20
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-10-16 22:44:55 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,2 +1,3 @@
 def solution(numbers):
-    return max(numbers[i] * numbers[j] for i in range(len(numbers)) for j in range(i + 1, len(numbers)))
+    numbers.sort()
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2] )
