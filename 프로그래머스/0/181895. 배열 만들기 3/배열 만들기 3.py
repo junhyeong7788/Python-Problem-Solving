ccd95339c8ad4022fe65561c3185b92eae5c29aa
JunHyeong <114178402+junhyeong7788@users.noreply.github.com>
2024-10-23 22:35:39 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.99 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,2 +1,2 @@
 def solution(arr, intervals):
-    return arr[intervals[0][0]:intervals[0][1]+1] + arr[intervals[1][0]:intervals[1][1]+1]
+    return [arr[i:j+1] for i, j in intervals][0] + [arr[i:j+1] for i, j in intervals][1]
